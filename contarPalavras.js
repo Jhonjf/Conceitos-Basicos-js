e29e1ed99e05b78cb215019a271505b04c92010b
Jhon <jhonatanjf0408@gmail.com>
2024-09-19 15:45:53 -0300

    Contar Palavras

---
+++ contarPalavras.js
@@ -0,0 +1,13 @@
+function contaPalavras(frase) {
+    const fraseLimpa = frase.trim();
+  
+    if (fraseLimpa === '') {
+      return 0;
+    }
+  
+    const palavras = fraseLimpa.split(/\s+/);
+    return palavras.length;
+  }
+  
+  console.log(contaPalavras('Olá, estou estudando para ser um furuto dev!'));
+  console.log(contaPalavras('Amo programar'))
